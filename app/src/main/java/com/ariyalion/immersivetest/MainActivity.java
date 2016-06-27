9d1ac06493d9b80ca7bc95089ba27ab00c5405a7
Bavan Palan <bavan@fetchtv.com.au>
2016-06-27 14:39:33 +1000

    Changing flags for better solution

--- app/src/main/java/com/ariyalion/immersivetest/MainActivity.java
+++ app/src/main/java/com/ariyalion/immersivetest/MainActivity.java
@@ -35,8 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (decorView != null) {
             decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_VISIBLE
             );
         }
     }
